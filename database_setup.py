310ffc306911a74ba91190ca7c61c7ec4933b6e9
Huey Kwik <huey.kwik@gmail.com>
2017-06-05 16:42:04 -0700

    Add user table.

--- database_setup.py
+++ database_setup.py
@@ -1,8 +1,19 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
 engine = create_engine('sqlite:///catalog.db')
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    picture = Column(String)
+
 
 Base.metadata.create_all(engine)
