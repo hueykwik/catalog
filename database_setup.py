4cb682087966f52125fd94febf0bff3a3e2d8bf0
Huey Kwik <huey.kwik@gmail.com>
2017-06-05 16:38:53 -0700

    Add skeleton code for database_setup.

---
+++ database_setup.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
