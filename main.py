bb684d9883dd58117aa76e8b3bd3cfc1692bb18c
Huey Kwik <huey.kwik@gmail.com>
2017-06-05 17:47:46 -0700

    Add catalog skeleton.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("main.html")
+    return render_template("catalog.html")
 
 if __name__ == '__main__':
     app.debug = True
