4ef929dfdfc3e72e34edf1d087b64b9e6c528082
Huey Kwik <huey.kwik@gmail.com>
2017-06-06 11:24:27 -0700

    Populate catalog with items.

--- populate_catalog.py
+++ populate_catalog.py
@@ -35,9 +35,30 @@ for category_name in _CATEGORIES:
 def get_category(session, category):
     return session.query(Category).filter_by(name=category).first()
 
-# soccer, baseball, frisbee, soccer, snowboarding, hockey
+# Use these categories with the items we create below.
 soccer = get_category(session, 'Soccer')
 baseball = get_category(session, 'Baseball')
 frisbee = get_category(session, 'Frisbee')
 snowboarding = get_category(session, 'Snowboarding')
 hockey = get_category(session, 'Hockey')
+
+# Stick (Hockey)
+# Goggles, Snowboard (Snowboarding)
+# Two shinguards, Shinguards, Jersey, Soccer Cleats (Soccer)
+# Frisbee (Frisbee)
+# Bat (Baseball)
+
+
+def create_item(name, description, category_id, user_id):
+    return Item(name=name, description=description, category_id=category_id, user_id=user_id)
+
+session.add(create_item('Stick', 'A hockey stick', hockey.id, user.id))
+session.add(create_item('Two Shinguards', 'Two shinguards. Not one.', soccer.id, user.id))
+session.add(create_item('Shinguard', 'Just one.', soccer.id, user.id))
+session.add(create_item('Jersey', 'My favorite jersey ever.', soccer.id, user.id))
+session.add(create_item('Soccer Cleats', 'These are shoes for soccer.', soccer.id, user.id))
+session.add(create_item('Frisbee', 'A frisbee. What did you expect?', frisbee.id, user.id))
+session.add(create_item('Bat', 'Me my friends and a baseball bat', baseball.id, user.id))
+session.commit()
+
+
